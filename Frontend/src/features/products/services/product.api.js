c0eae146690138e0906e68b2bd84e06861bc473b
Jayprakash Narayan <jprakashlucky05@gmail.com>
2026-09-19 23:43:12 +0530

    feat(product): create product API service with create and getSellerProduct functions

---
+++ Frontend/src/features/products/services/product.api.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+const productApiInstance=axios.create({
+    baseURL:"/api/products",
+    withCredentials:true,
+})
+
+export async function createProduct(formdata){
+
+    const response = await productApiInstance.post("/",formdata)
+
+    return response.data
+
+}
+
+export async function getSellerProduct(){
+    const response = await productApiInstance.get("/seller")
+    
+    return response.data
+}
+
+
+export default productApiInstance
